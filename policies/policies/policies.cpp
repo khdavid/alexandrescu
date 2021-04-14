8fd0940255742cc2c756e82ac96cab8d17ee9351
David Khudaverdyan <david.khudaverdyan@bentley.com>
2021-04-14 08:49:33 +0200

    create policy classes

--- policies/policies/policies.cpp
+++ policies/policies/policies.cpp
@@ -3,6 +3,52 @@
 
 #include <iostream>
 
+template <class T>
+struct OpNewCreator
+{
+  static T* Create()
+  {
+    return new T;
+  }
+};
+
+template <class T>
+struct MallocCreator
+{
+  static T* Create()
+  {
+    void* buf = std::malloc{sizeof(T) };
+    if (!buf)
+    {
+      return nullptr;
+    }
+    return new(buf) T;
+  }
+};
+
+template <class T>
+struct PrototypeCreator
+{
+  PrototypeCreator(T * pObj = nullptr) : pPrototype_(pObj)
+  {
+
+  }
+  T* Create()
+  {
+    return pPrototype_ ? pPrototype_->Clone() : NULL;
+  }
+  T* GetPrototype()
+  {
+    return pPrototype_;
+  }
+  void SetPrototype(T *pObj)
+  {
+    pPrototype_ = pObj;
+  }
+private:
+  T pPrototype_;
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
